62ffd836353f4dac78b5222687a57fc3888f9448
SAOT31 <saot-31@hotmail.com>
2026-01-30 15:43:39 -0500

    feat: initialize api constants and dom selectors for auth

---
+++ login-auth.js
@@ -0,0 +1,6 @@
+// API configuration - Pointing to your local JSON Server
+const API_URL = "http://localhost:3000/users";
+
+// DOM Elements selection for Login and Register sections
+const loginSec = document.getElementById('login-section');
+const regSec = document.getElementById('register-section');
